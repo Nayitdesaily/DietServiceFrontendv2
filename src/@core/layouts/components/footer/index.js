8d624527fe60389303a05264da8b2ce7b5d1d465
Nayit Ruiz Jaramillo <nayit.ruiz@grupogenera.pe>
2023-08-16 17:42:22 -0500

    actualizacion del footer y nombre en navegacion

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -6,14 +6,15 @@ const Footer = () => {
     <p className='clearfix mb-0'>
       <span className='float-md-start d-block d-md-inline-block mt-25'>
         COPYRIGHT © {new Date().getFullYear()}{' '}
-        <a href='https://1.envato.market/pixinvent_portfolio' target='_blank' rel='noopener noreferrer'>
-          Pixinvent
+        <a href='https://dietservice.pe/' target='_blank' rel='noopener noreferrer'>
+          DietService
         </a>
-        <span className='d-none d-sm-inline-block'>, All rights Reserved</span>
+        <span className='d-none d-sm-inline-block'>, Todos los derechos reservados</span>
       </span>
       <span className='float-md-end d-none d-md-block'>
-        Hand-crafted & Made with
-        <Heart size={14} />
+        Diseño y Desarrollo, <a href='https://bitjoins.pe/' target='_blank' rel='noopener noreferrer'>
+          Bitjoins
+        </a>
       </span>
     </p>
   )
--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -6,7 +6,7 @@ import logo from '@src/assets/images/logo/logo.svg'
 //Template config options
 const themeConfig = {
   app: {
-    appName: 'Diet Service',
+    appName: '',
     appLogoImage: logo
   },
   layout: {
